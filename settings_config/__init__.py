f0d38c4751e07f2b8f3a8bddea5910e7f4d33911
Prithviraj8 <prithvimurthy8@gmail.com>
2023-01-26 13:53:46 +0530

    fix firebase initializing

--- settings_config/__init__.py
+++ settings_config/__init__.py
@@ -1,13 +1,26 @@
-from services.firebase_cloud_client import FirebaseClient
+import os
+
+import firebase_admin
+from firebase_admin import credentials
 
 
 def initialize_firebase_client():
     global firebase_client
     firebase_client = None
-    firebase_client_obj = FirebaseClient()
-    if not firebase_client:
-        firebase_client = firebase_client_obj.get_firebase_client
-    return firebase_client
+
+    if not firebase_admin._apps:
+        print('Initializing Firebase Client')
+        cred = credentials.Certificate(
+            os.path.abspath(os.path.dirname(__file__)) + "/firebase_config.json"
+        )
+        return firebase_admin.initialize_app(cred)
+
+    # global firebase_client
+    # firebase_client = None
+    # firebase_client_obj = FirebaseClient()
+    # if not firebase_client:
+    #     firebase_client = firebase_client_obj.get_firebase_client
+    # return firebase_client
 
 
 firebase_client = initialize_firebase_client()
