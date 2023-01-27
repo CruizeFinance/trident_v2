37002a9e92253a47dc7ff67a0f0087fdb5f914f4
Prithviraj8 <prithvimurthy8@gmail.com>
2023-01-27 13:11:55 +0530

    fix firebase initializing

--- settings_config/settings.py
+++ settings_config/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = [
     "34.225.2.31",
     "beta.trident.v2.cruize.finance",
     "localhost",
+    "http://localhost:3000",
 ]
 
 # Application definition
@@ -58,6 +59,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "vaults",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -68,6 +70,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "settings_config.urls"
