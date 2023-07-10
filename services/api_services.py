69ce45dc1fc250e8e2071c037d0518b9c9cb589d
Prithviraj Murthy <31995793+Prithviraj8@users.noreply.github.com>
2023-07-10 16:19:03 +0530

    Revert "Use Coingecko api to get monday price"

--- services/api_services.py
+++ services/api_services.py
@@ -1,59 +1,17 @@
-import pytz
 import requests
 
 from utilities import constant
-from dateutil.relativedelta import relativedelta, MO
-from datetime import timedelta, datetime, date
-
-
-def asset_price_coingecko_historical(asset_name, current_day):
-    # https://api.coingecko.com/api/v3/coins/bitcoin/history?date=10-07-2023&localization=false
-    monday_datetime = str(
-        (current_day + relativedelta(weekday=MO(-1))).strftime("%d-%m-%Y")
-    )
-
-    url = (
-        constant.COINGECKO_HOST
-        + f"/coins/{constant.symbol_asset[asset_name]}/history?date={monday_datetime}&localization=false"
-    )
-    try:
-        result = dict(requests.get(url).json())
-        asset_price = result["market_data"]["current_price"]["usd"]
-        return asset_price
-    except Exception as e:
-        raise Exception(str(e))
 
 
 def asset_price(asset_name):
-    url = constant.COINBASE_HOST + f"prices/{asset_name}-USD/spot"
-
+    url = constant.COINGECKO_HOST + f"/simple/price?ids={asset_name}&vs_currencies=usd"
     try:
         result = dict(requests.get(url).json())
-        asset_price = result["data"]["amount"]
-        return asset_price
-    except Exception as e:
-        raise Exception(str(e))
-
-
-def asset_price_historic(asset_name, time="09:00", day=None):
-    monday = 0
-    day_diff = day - monday
-
-    url = constant.COINBASE_HOST + f"prices/{asset_name}-USD/historic?days={day_diff}"
-    try:
-        result = dict(requests.get(url).json())
-        prices = result["data"]["prices"]
-        return prices
-        # for price in prices:
-        #     if time in price['time']:
-        #         pass
-
+        asset_price = result[asset_name]["usd"]
         return asset_price
     except Exception as e:
         raise Exception(str(e))
 
 
 if __name__ == "__main__":
-    # print(asset_price_historic(asset_name="ETH", day=200))
-    # print(binance_get_historical_data(5))
-    print(asset_price_coingecko_historical("WBTC", date.today()))
+    asset_price("ethereum")
